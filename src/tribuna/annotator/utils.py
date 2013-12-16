fcb330d27f33a5561f806a52f7a8c18cb995352d
Termitnjak d.o.o <info@termitnjak.si>
2013-12-16 17:22:01 +0000

    add a message that verbose security needs to be turned on to make unrestricted_create work, XXX

--- src/tribuna/annotator/utils.py
+++ src/tribuna/annotator/utils.py
@@ -15,6 +15,9 @@ def unrestricted_create(container=None, portal_type=None,
     XXX: this would be a bit cleaner if we used api.env.adopt_roles,
     but it doesn't seem to work properly..
 
+    XXX 2: Verbose security needs to be turned on in buildout to make
+    this method work. WTF, Plone! We need to fix this.
+
     :param container: container for the created object
     :param portal_type: type of the object to create
     :param id: id of the object to create
